a8edc363785588b13a22bd2db9a3720cc5d1d99d
Taeho Kim <tkim@lyft.com>
2018-08-11 23:09:05 -0700

    remove Pods

--- app/ios/Family/Pods/Target Support Files/Pods-FamilyUITests/Pods-FamilyUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_FamilyUITests : NSObject
-@end
-@implementation PodsDummy_Pods_FamilyUITests
-@end
---
+++ app/ios/Family/README.md
@@ -0,0 +1,5 @@
+# Family iOS
+
+```
+$ make pod_install
+```
